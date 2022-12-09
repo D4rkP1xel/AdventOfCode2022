110495554af54ca91e42236834610f70b84de75a
Alex <alexcruzsilva12@gmail.com>
2022-12-09 19:48:24 +0000

    im a fucking god

---
+++ day9/day9part1.ts
@@ -0,0 +1,111 @@
+import { readFile } from 'fs/promises'
+
+async function app() {
+    const data: string = await readFile('./day9/input.txt', { encoding: 'utf8' })
+    const inputData: string[] = data.split('\n').map((l) => { return l.trim() })
+    interface position {
+        x: number,
+        y: number
+    }
+    let tailPositionsVisited: position[] = [{ x: 0, y: 0 }]
+    let currentHeadPosition: position = { x: 0, y: 0 }
+    let currentTailPosition: position = { x: 0, y: 0 }
+
+
+
+    for (let line = 0; line < inputData.length; line++) {
+        let direction = inputData[line].split(" ")[0]
+        let steps = parseInt(inputData[line].split(" ")[1])
+        moveHead(currentHeadPosition, currentTailPosition, direction, steps)
+    }
+    console.log(currentHeadPosition, currentTailPosition)
+    console.log(tailPositionsVisited.length)
+
+    function moveHead(headPos: position, tailPos: position, direction: string, steps: number): void {
+
+        if (direction === "U") {
+            for (let i = 0; i < steps; i++) {
+                headPos.y++
+                moveTail(headPos, tailPos)
+            }
+            return
+        }
+        if (direction === "D") {
+            for (let i = 0; i < steps; i++) {
+                headPos.y--
+                moveTail(headPos, tailPos)
+            }
+            return
+        }
+        if (direction === "L") {
+            for (let i = 0; i < steps; i++) {
+                headPos.x--
+                moveTail(headPos, tailPos)
+            }
+            return
+        }
+        if (direction === "R") {
+            for (let i = 0; i < steps; i++) {
+                headPos.x++
+                moveTail(headPos, tailPos)
+
+            }
+            return
+        }
+        return console.log("\n\n\nERROR\n\n\n")
+    }
+    function moveTail(headPos: position, tailPos: position) {
+
+        if ((tailPos.x === headPos.x && tailPos.y === headPos.y) ||
+            (headPos.x === tailPos.x && Math.abs(headPos.y - tailPos.y) === 1) ||
+            (headPos.y === tailPos.y && Math.abs(headPos.x - tailPos.x) === 1) ||
+            (Math.abs(headPos.y - tailPos.y) === 1 && Math.abs(headPos.x - tailPos.x) === 1)
+        ) return //tail doesn't need to move
+
+        if (tailPos.x - headPos.x === -2) //left side
+        {
+            tailPos.x++
+            tailPos.y = headPos.y
+
+            addToPositionsVisited(tailPos)
+
+            return
+        }
+        if (tailPos.x - headPos.x === 2) //right side
+        {
+            tailPos.x--
+            tailPos.y = headPos.y
+            addToPositionsVisited(tailPos)
+
+            return
+        }
+        if (tailPos.y - headPos.y === -2) //bottom side
+        {
+            tailPos.y++
+            tailPos.x = headPos.x
+            addToPositionsVisited(tailPos)
+            return
+        }
+        if (tailPos.y - headPos.y === 2) //up side
+        {
+            tailPos.y--
+            tailPos.x = headPos.x
+            addToPositionsVisited(tailPos)
+            return
+        }
+        return console.log("\n\nERROR2", headPos, tailPos, "\n\n\n")
+    }
+
+
+    function addToPositionsVisited(tailPos: position) {
+
+        for (let i = 0; i < tailPositionsVisited.length; i++) {
+            if ((tailPositionsVisited[i].x === tailPos.x) && (tailPositionsVisited[i].y === tailPos.y)) {
+                return
+            }
+        }
+        tailPositionsVisited.push({ x: tailPos.x, y: tailPos.y })
+    }
+}
+
+app()
